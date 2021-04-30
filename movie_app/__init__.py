b29f217652f6398de33bdb7dde905200570dbb3a
lavuna <ivan.manchur47@gmail.com>
2021-04-30 23:38:52 +0300

    Fixed some bugs

--- movie_app/__init__.py
+++ movie_app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 app.config.from_pyfile('./../instance/flask.cfg')
 
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
