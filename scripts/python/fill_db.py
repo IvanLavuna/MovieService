1653716b88b8cdc2c24584acebae35205d9a58bc
lavuna <ivan.manchur47@gmail.com>
2021-04-30 23:27:17 +0300

    modified script

--- scripts/python/fill_db.py
+++ scripts/python/fill_db.py
@@ -6,19 +6,11 @@ session = db_session()
 data = \
 [
     [
-        User(id=1, username="LaVuna",    firstname="Ivan",   lastname="Manchur",   email="LaVuna@gmail.com",    password_hash="lavuna2002",    phone_number="90123123",   photo="photo.jpeg"),
-        User(id=2, username="Pikachu",   firstname="Borys",  lastname="Laplas",    email="Lapko@gmail.com",     password_hash="laplap2002",    phone_number="901231233",  photo="photo.jpeg"),
-        User(id=3, username="Hulk",      firstname="Ostap",  lastname="Manchur",   email="trololol@gmail.com",  password_hash="ostap19991999", phone_number="517294381",  photo="photo.jpeg"),
-        User(id=4, username="Padon",     firstname="Julian", lastname="*********", email="Padon@gmail.com",     password_hash="qwerty123",     phone_number="872368923",  photo="photo.jpeg"),
-        User(id=5, username="AdrianZP",  firstname="Adrian", lastname="Bilder",    email="SubAndDub@gmail.com", password_hash="all123",        phone_number="90121233123",photo="photo.jpeg")
-    ],
-    [
-        Movie(id=1, name="Matrix", picture="KianyRivs.jpeg", info="Psychological Movie about world", actors="Kiany Rivs, Jennie Eshley",
-                duration="2:11:26"),
-        Movie(id=2, name="Matrix:Reboot", picture="KianyRivs.jpeg", info="Psychological Movie about world", actors="Kiany Rivs, Jennie Eshley",
-                duration="2:11:26"),
-        Movie(id=3, name="Matrix:Revolution", picture="KianyRivs.jpeg", info="Psychological Movie about world", actors="Kiany Rivs, Jennie Eshley",
-                duration="2:11:26")
+        User(id=1, username="LaVuna",    firstname="Ivan",   lastname="Manchur",   email="LaVuna@gmail.com",    password_hash=User.hash_password("lavuna2002"),    phone_number="90123123",   photo="photo.jpeg"),
+        User(id=2, username="Pikachu",   firstname="Borys",  lastname="Laplas",    email="Lapko@gmail.com",     password_hash=User.hash_password("laplap2002"),    phone_number="901231233",  photo="photo.jpeg"),
+        User(id=3, username="Hulk",      firstname="Ostap",  lastname="Manchur",   email="trololol@gmail.com",  password_hash=User.hash_password("ostap19991999"), phone_number="517294381",  photo="photo.jpeg"),
+        User(id=4, username="Padon",     firstname="Julian", lastname="*********", email="Padon@gmail.com",     password_hash=User.hash_password("qwerty123"),     phone_number="872368923",  photo="photo.jpeg"),
+        User(id=5, username="AdrianZP",  firstname="Adrian", lastname="Bilder",    email="SubAndDub@gmail.com", password_hash=User.hash_password("all123"),        phone_number="90121233123",photo="photo.jpeg")
     ],
     [
         MovieSchedule(id=1,  movie_id=1,  date="1999-03-14", time="12:53:42"),
